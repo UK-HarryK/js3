0493097c141b0495c3bbfb980100303e909c8373
UK-HarryK <harrykilbride7@gmail.com>
2021-10-20 15:54:12 +0100

    finished tasks

---
+++ script.js
@@ -0,0 +1,34 @@
+// baseNum = Math.floor(Math.random()*100)
+// alert(baseNum)
+// function determineOddEven(number){
+//     remainder = number % 2
+//     if(remainder == 0){
+//         alert("This number is even")
+//     } else{
+//         alert("This number is odd")
+//     }
+// }
+// determineOddEven(baseNum)
+
+num1 = prompt("Provide a number")
+num2 = prompt("Provide a number")
+function division(num1, num2){
+    let remainder = num1 % num2
+    let value = Math.floor(num1 / num2)
+    if(remainder === 0){
+        alert(`${num1} is wholly divisable into ${num2}`)
+    } else{
+        alert(`${num1} divides into ${num2} to make ${value} with ${remainder} remaining`)
+    }
+}
+division(num1, num2)
+
+students = parseInt(prompt("How many students"))
+busCapacity = parseInt(prompt("How many people can fit on the bus?"))
+
+function howManyBuses(students, busCapacity){
+    let numOfBuses = Math.ceil(students/busCapacity)
+    alert(`You need ${numOfBuses} buses to get the students to where they are going`)
+}
+
+howManyBuses(students, busCapacity)
